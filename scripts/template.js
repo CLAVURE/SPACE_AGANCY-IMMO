724c4a1a352bd6de0c828205e0307248a7f488b0
vovArks Clavure <darkroiduchocapic@gmail.com>
2022-12-02 17:31:54 +0100

    02/12 fin cours soir

--- scripts/template.js
+++ scripts/template.js
@@ -1,9 +1,9 @@
-//Font Etoilé
+//Fond étoilé
 particlesJS(
     "particles-js", {
          "particles": 
          { "number": 
-         { "value": 1260, "density": { "enable": true, "value_area": 800 } },
+         { "value": 400, "density": { "enable": true, "value_area": 800 } },
           "color": { "value": "#ffffff" },
            "shape": { 
                 "type": "circle", 
@@ -16,7 +16,7 @@ particlesJS(
         "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 4, "size_min": 0.3, "sync": false } },
         "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
         "move": { 
-            "enable": true, "speed": 1,
+            "enable": true, "speed": 0.7,
             "direction": "none", 
             "random": true, "straight": false,
             "out_mode": "out", "bounce": false, 
@@ -29,9 +29,9 @@ particlesJS(
                 "resize": true },
             "modes": { 
                 "grab": { 
-                    "distance": 400, 
+                    "distance": 100, 
                     "line_linked": { "opacity": 1 } }, 
-                    "bubble": { "distance": 100, "size": 0, "duration": 2, "opacity": 1, "speed": 3}, 
+                    "bubble": { "distance": 100, "size": 9, "duration": 2, "opacity": 1, "speed": 3}, 
                     "repulse": { "distance": 200, "duration": 0.4 }, 
                     "push": { "particles_nb": 10 }, 
                     "remove": { "particles_nb": 2 } } 
