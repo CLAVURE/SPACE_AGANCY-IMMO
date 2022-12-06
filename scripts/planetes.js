8b4dbab6e9ee6e09052fcb7f2cdb063f4dafa381
vovArks Clavure <darkroiduchocapic@gmail.com>
2022-12-06 09:26:31 +0100

    09:26 06 12

---
+++ scripts/planetes.js
@@ -0,0 +1,22 @@
+let system = [
+    {"type":"header","version":"5.1.1","comment":"Export to JSON plugin for PHPMyAdmin"},
+    {"type":"database","name":"planetes"},
+    {"type":"table","name":"planetes","database":"planetes","data":[{
+        "nom":"VENUS",
+        "description":"Vénus est la deuxième planète du Système solaire par ordre d'éloignement au Soleil, et la sixième plus grosse aussi bien par la masse que le diamètre. Elle doit son nom à la déesse romaine de l'amour.",
+        "couleur1":"#F6EDE7",
+        "couleur2":"#E5C39A",
+        "couleur3":"#9B876E",
+        "couleur4":"#080312",
+        "img":"venus.svg"},
+    {
+        "nom":"GLIESE 667CC",
+        "description":"Gliese 667 Cc3,1,4 (également connue sous la dénomination GJ 667Cc ou HR 6426Cc) est une planète extrasolaire (exoplanète) confirmée de type super-Terre, en orbite autour de l'étoile Gliese 667 C, une naine rouge qui est elle-même un membre du système de trois étoiles Gliese 667 situé à une distance d'environ 22,7 années-lumière, dans la constellation zodiacale du Scorpion.",
+        "couleur1":"#FBB77A",
+        "couleur2":"#755137",
+        "couleur3":"#273436",
+        "couleur4":"#080312",
+        "img":"gliese.svg"}
+    ]}
+]
+    
