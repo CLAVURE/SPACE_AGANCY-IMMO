00f9597ae0fd6080d4340888247df410bb081734
vovArks Clavure <darkroiduchocapic@gmail.com>
2023-01-12 20:58:21 +0100

    12 01 20:58

--- scripts/plans.js
+++ scripts/plans.js
@@ -354,6 +354,8 @@ let mdr = {
 
     document.querySelector('.nomPlanete').innerHTML = document.location.href.split('=')[1];
 
+    document.querySelector('.plansDe>span').innerHTML = document.location.href.split('=')[1];
+
     document.querySelector('.listeBiens').innerHTML=`
     <svg class="SVGup" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="245.641" height="293.911" viewBox="0 0 368.462 440.866"><defs><pattern id="a" width="1" height="1" viewBox="-7.567 0 102.573 95.979"><image preserveAspectRatio="xMidYMid slice" width="91.517" height="91.517"  xlink:href="../img/Plans/` + document.location.href.split('=')[1] + `/1.png"/></pattern><pattern id="b" width="1" height="1" viewBox="1.971 0 106.89 99.366"><image class="b" preserveAspectRatio="xMidYMid slice" width="110.832" height="99.366" xlink:href="../img/Plans/` + document.location.href.split('=')[1] + `/2.png"/></pattern><pattern id="c" preserveAspectRatio="xMidYMid slice" width="100%" height="100%" viewBox="0 0 1024 1024"><image class="c" width="1024" height="1024" xlink:href="../img/Plans/` + document.location.href.split('=')[1] + `/3.png"/></pattern><pattern id="d" width="1" height="1" viewBox="0 -0.385 99.811 100.582"><image preserveAspectRatio="xMidYMid slice" width="99.811" height="99.811" xlink:href="../img/Plans/` + document.location.href.split('=')[1] + `/4.png"/></pattern><pattern id="e" width="1" height="1" viewBox="0 0.698 95.045 93.649"><image preserveAspectRatio="xMidYMid slice" width="95.045" height="95.045" xlink:href="../img/Plans/` + document.location.href.split('=')[1] + `/5.png"/></pattern><pattern id="f" preserveAspectRatio="xMidYMid slice" width="100%" height="100%" viewBox="0 0 1024 1024"><image width="1024" height="1024" xlink:href="../img/Plans/` + document.location.href.split('=')[1] + `/6.png"/></pattern></defs><g transform="translate(-365.556 -347.127)"><line x2="116.302" y2="131" transform="translate(421.539 408.123)" fill="none" stroke="#fff" stroke-width="2"/><line x2="119.497" y2="88.185" transform="translate(558.29 560.85)" fill="none" stroke="#fff" stroke-width="2"/><line y1="3.834" x2="224.297" transform="translate(433.68 392.787)" fill="none" stroke="#fff" stroke-width="2"/><line x2="10.224" y2="221.741" transform="translate(679.065 413.874)" fill="none" stroke="#fff" stroke-width="2"/><line y1="66.458" x2="108.634" transform="translate(580.655 679.708)" fill="none" stroke="#fff" stroke-width="2"/><line x1="115.663" y1="77.961" transform="translate(420.9 666.288)" fill="none" stroke="#fff" stroke-width="2"/><line y1="214.072" x2="1.278" transform="translate(410.037 417.07)" fill="none" stroke="#fff" stroke-width="2"/><path id=` + scope[1] + ` d="M0,42.817,46.692,0,91.517,40.67v4.317L47.19,85.633Z"  transform="translate(365.556 353.993)" fill="url(#a)"/><path id=` + scope[2] + ` d="M3882.775-3354.278l-29.891-29.891-17.385-17.385,52.09-52.09,54.8,54.8v1.369l-43.2,43.2Z" transform="translate(-3209.879 3800.771)" fill="url(#b)"/><path id=` + scope[3] + ` d="M3864.539-3371.007l-42.2-42.2v-5.809l45.848-45.849,48.152,48.152v5.811l-39.9,39.9Z" transform="translate(-3319.905 3970)" fill="url(#c)"/><path id=` + scope[4] + ` d="M3824.384-3415.12l48.753-48.754,50.293,50.293v1.53l-47.989,47.988Z" transform="translate(-3189.412 4077)" fill="url(#d)"/><path id=` + scope[5] + ` d="M3877.28-3357.007l-44.9-44.9,48.753-48.753,46.292,46.292v9.531l-37.826,37.826Z" transform="translate(-3324 4145)" fill="url(#e)"/><path id=` + scope[6] + ` d="M3858.287-3377.007l-31.114-31.114v-15.486l41.01-41.01,46.99,46.99v8.136l-32.484,32.484Z" transform="translate(-3449.634 4083.843)" fill="url(#f)"/></g></svg>
     `;
@@ -385,18 +387,18 @@ let mdr = {
 
         document.querySelector('.nomPlanete').classList.add('displayNone');
         document.querySelector('.listeBiens').classList.add('displayNone');
-        document.querySelector('p').classList.add('displayNone');document.querySelector('.Bien').classList.add('displayBlock');
+        document.querySelector('.cliquez').classList.add('displayNone');document.querySelector('.Bien').classList.add('displayBlock');
         setTimeout(listeDisparait, 550);
         function listeDisparait(){
             document.querySelector('.nomPlanete').style.display = 'none';
             document.querySelector('.listeBiens').style.display = 'none';
-            document.querySelector('p').style.display = 'none';
+            document.querySelector('.cliquez').style.display = 'none';
         
             document.querySelector('.Bien').style.opacity = '100%';
 
             document.querySelector('.nomPlanete').classList.remove('displayNone');
             document.querySelector('.listeBiens').classList.remove('displayNone');
-            document.querySelector('p').classList.remove('displayNone');
+            document.querySelector('.cliquez').classList.remove('displayNone');
             document.querySelector('.Bien').classList.remove('displayBlock');
 
         }
@@ -407,19 +409,19 @@ let mdr = {
     function fermerPlan(){
         document.querySelector('.nomPlanete').classList.add('displayBlock');
         document.querySelector('.listeBiens').classList.add('displayBlock');
-        document.querySelector('p').classList.add('displayBlock');
+        document.querySelector('.cliquez').classList.add('displayBlock');
         document.querySelector('.Bien').classList.add('displayNone');
         setTimeout(listeApparait, 550);
         function listeApparait(){
             document.querySelector('.nomPlanete').style.display = 'flex';
             document.querySelector('.listeBiens').style.display = 'flex';
-            document.querySelector('p').style.display = 'flex';
+            document.querySelector('.cliquez').style.display = 'flex';
 
             document.querySelector('.Bien').style.opacity = '0';
 
             document.querySelector('.nomPlanete').classList.remove('displayBlock');
             document.querySelector('.listeBiens').classList.remove('displayBlock');
-            document.querySelector('p').classList.remove('displayBlock');
+            document.querySelector('.cliquez').classList.remove('displayBlock');
             document.querySelector('.Bien').classList.remove('displayNone');
         }
     }
